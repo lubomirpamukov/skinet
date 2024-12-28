b4216e17ce2a8a24eca23ab8535a5084d2ccad79
lubomirpamukov <lubomir.pamukov@gmail.com>
2024-12-28 19:08:22 +0200

    Added Pagination properties and criteria method

--- Core/Interfaces/ISpecification.cs
+++ Core/Interfaces/ISpecification.cs
@@ -9,6 +9,11 @@ public interface ISpecification<T>
     Expression<Func<T, object>>? OrderBy {get;}
     Expression<Func<T, object>>? OrderByDescending {get;}
     bool IsDistinct {get;}
+    int Skip{get;}
+    int Take{get;}
+    bool IsPagingEnabled {get;}
+
+    IQueryable<T>ApplyCriteria(IQueryable<T>query);
 }
 
 public interface ISpecification<T, TResult> : ISpecification<T>
