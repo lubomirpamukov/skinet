9b4fbd4d1600de9979a20ec68234fc84dac3c09b
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-01-28 15:45:10 +0200

    rout config for errors

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { ShopComponent } from './features/shop/shop.component';
 import { ProductDetailsComponent } from './features/shop/product-details/product-details.component';
+import { TestErrorComponent } from './features/test-error/test-error.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent, title: 'Home'},
     {path: 'shop', component: ShopComponent, title: 'Shop'},
     {path: 'shop/:id', component: ProductDetailsComponent, title: 'Product Details'},
+    {path: 'test-error', component: TestErrorComponent, title: 'Error Test'},
     {path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
