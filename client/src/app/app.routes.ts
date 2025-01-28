712dcd41b5b9d7d282fa5d5f2ddb52998d1b3b7b
Lubomir Pamukov <lubomir.pamukov@gmial.com>
2025-01-28 10:32:28 +0200

    implement routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { ShopComponent } from './features/shop/shop.component';
+import { ProductDetailsComponent } from './features/shop/product-details/product-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent, title: 'Home'},
+    {path: 'shop', component: ShopComponent, title: 'Shop'},
+    {path: 'shop/:id', component: ProductDetailsComponent, title: 'Product Details'},
+    {path: '**', redirectTo: 'home', pathMatch: 'full'}
+];
