59d0c06cbbf3504b7ad2b28aa4dcd66825572923
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-02-18 15:19:40 +0200

    implemented taking cart from local storage and checking redis database to retrive user cart

--- client/src/app/core/services/cart.service.ts
+++ client/src/app/core/services/cart.service.ts
@@ -1,8 +1,9 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal, Pipe, PipeTransform } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Cart, CartItem } from '../../shared/models/carts';
 import { Product } from '../../shared/models/products';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,9 +16,12 @@ export class CartService {
   constructor() { }
 
   getCart(id: string){
-    return this.http.get<Cart>(this.baseUrl + 'cart?id=' + id).subscribe({
-      next : cart => this.cart.set(cart)
-    })
+    return this.http.get<Cart>(this.baseUrl + 'cart?id=' + id).pipe(
+      map(cart => {
+        this.cart.set(cart)
+        return cart
+      })
+    )
   }
 
   setCart(cart: Cart){
