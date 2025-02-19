59ce537433af95f5d00fbb66786a25c1ff57cef5
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-02-19 13:40:53 +0200

    generated empthy checkout component and service

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -6,12 +6,14 @@ import { TestErrorComponent } from './features/test-error/test-error.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 import { ServerErrorComponent } from './shared/components/server-error/server-error.component';
 import { CartComponent } from './features/cart/cart.component';
+import { CheckoutComponent } from './features/checkout/checkout.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent, title: 'Home'},
     {path: 'shop', component: ShopComponent, title: 'Shop'},
     {path: 'shop/:id', component: ProductDetailsComponent, title: 'Product Details'},
     {path: 'cart', component: CartComponent, title: 'Cart'},
+    {path: 'checkout', component: CheckoutComponent, title: 'Checkout'},
     {path: 'test-error', component: TestErrorComponent, title: 'Error Test'},
     {path: 'not-found', component: NotFoundComponent, title: 'Not found'},
     {path: 'server-error', component: ServerErrorComponent, title: 'Server error'},
