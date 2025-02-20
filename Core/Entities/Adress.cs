55519a0ed78575b407f713649aff1ea08a48fabe
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-02-20 17:48:15 +0200

    added user address class and migrate it to the database

---
+++ Core/Entities/Adress.cs
@@ -0,0 +1,16 @@
+namespace Core.Entities;
+
+public class Address : BaseEntity
+{
+    public required string Line1 { get; set; }
+
+    public required string? Line2 { get; set; }
+
+    public required string City { get; set; }
+
+    public required string? State { get; set; }
+
+    public required string PostalCode { get; set; }
+
+    public required string Country { get; set; }
+}
