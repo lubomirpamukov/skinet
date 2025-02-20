65d4c573fd9c4e334e5cc2617eb4cf39611c168e
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-02-20 22:02:00 +0200

    added routes for login and register components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -7,6 +7,8 @@ import { NotFoundComponent } from './shared/components/not-found/not-found.compo
 import { ServerErrorComponent } from './shared/components/server-error/server-error.component';
 import { CartComponent } from './features/cart/cart.component';
 import { CheckoutComponent } from './features/checkout/checkout.component';
+import { LoginComponent } from './features/account/login/login.component';
+import { RegisterComponent } from './features/account/register/register.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent, title: 'Home'},
@@ -14,6 +16,8 @@ export const routes: Routes = [
     {path: 'shop/:id', component: ProductDetailsComponent, title: 'Product Details'},
     {path: 'cart', component: CartComponent, title: 'Cart'},
     {path: 'checkout', component: CheckoutComponent, title: 'Checkout'},
+    {path: 'login', component: LoginComponent, title: 'Login'},
+    {path: 'register', component: RegisterComponent, title: 'Register'},
     {path: 'test-error', component: TestErrorComponent, title: 'Error Test'},
     {path: 'not-found', component: NotFoundComponent, title: 'Not found'},
     {path: 'server-error', component: ServerErrorComponent, title: 'Server error'},
