5df0d06a3eba2296674ae32e41d8b9eb48867113
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-02-20 21:16:28 +0200

    fixed account/user-info endpoint to return adress as well

--- API/DTO/AdressDto.cs
+++ API/DTO/AdressDto.cs
@@ -6,14 +6,17 @@ public class AddressDto
 {   
     [Required]
     public  string Line1 { get; set; } = null!;
-    [Required]
+
     public  string? Line2 { get; set; }
+    
     [Required]
     public  string City { get; set; } = null!;
-    [Required]
+
     public string? State { get; set; }
+
     [Required]
     public  string PostalCode { get; set; } = null!;
+
     [Required]
     public  string Country { get; set; } = null!;
 }
