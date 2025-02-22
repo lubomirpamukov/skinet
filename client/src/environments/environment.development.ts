e941d43d381946eccaeb5ed987d46e6eebbe92ca
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-02-22 23:06:17 +0200

    Added stripe service

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5001/api/'
+    apiUrl: 'https://localhost:5001/api/',
+    stripePublicKey: 'pk_test_51QvH5MG3yf0wwHLG5zn8lyV1im8SDTH1HHVHhh74D7GV7X6J2mKPSZMDE2GXJ7rHIrV4evEzF6RGpTWQlraOE8vw00yJc8jfzv'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'api/'
+    apiUrl: 'api/',
+    stripePublicKey: 'pk_test_51QvH5MG3yf0wwHLG5zn8lyV1im8SDTH1HHVHhh74D7GV7X6J2mKPSZMDE2GXJ7rHIrV4evEzF6RGpTWQlraOE8vw00yJc8jfzv'
 };
